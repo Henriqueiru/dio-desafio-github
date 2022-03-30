d841257ae48d61b09a219025813f4847e28086a1
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-03-30 16:32:12 -0300

    Aprendendo Operador IF

---
+++ Estudos C# em código/EstudosCsharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection.Metadata;
+using System;
+
+namespace EstudosCsharp
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 2;
+      double number = 25.94;
+
+      number = x;
+
+    }
+  }
+}
+
---
+++ Estudos C# em código/Usando IF/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+namespace Usando_IF
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+
+      int idade = 21;
+      int maioridade = 21;
+      int idadeMaxima = 65;
+      if (idade >= maioridade || idade < idadeMaxima)
+      {
+        Console.WriteLine("Ele é de maior");
+      }
+      else
+      {
+        Console.WriteLine("É de menor");
+      }
+      Console.WriteLine("Finalizou o programa");
+    }
+  }
+}
