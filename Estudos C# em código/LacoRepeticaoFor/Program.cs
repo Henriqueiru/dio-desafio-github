b826df9cbae29388d65b95462ff736a128a18844
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-03-30 17:06:07 -0300

    Aprendendo Laço de repetição for

---
+++ Estudos C# em código/LacoRepeticaoFor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LacoRepeticaoFor
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      for (int i = 1; i <= 5; i++)
+      {
+        Console.WriteLine(i);
+      }
+    }
+  }
+}
