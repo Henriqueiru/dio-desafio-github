c431b62dd93690ffdfa4b4ed67e4d1af3fde6423
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-03-30 17:17:28 -0300

    Aprendendo Laço de repetição While

---
+++ Estudos C# em código/LacoRepeticaoWhile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LacoRepeticaoWhile
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int valor = 0;
+      while (true)
+      {
+        valor++;
+        Console.WriteLine(valor);
+
+      }
+    }
+  }
+}
