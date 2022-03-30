7d530d4d229429d01e278b8655fcd60e2f7b788a
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-03-30 17:55:19 -0300

    Aprendendo metodos

---
+++ Estudos C# em código/MetodosEfuncoes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+namespace MetodosEfuncoes
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      MeuMetodo();
+
+      string nome = RetornaNome("André", "Baltieri");
+      Console.WriteLine(nome);
+    }
+
+    static void MeuMetodo()
+    {
+      Console.WriteLine("C# é legal");
+    }
+
+    static string RetornaNome(
+      string nome,
+      string sobrenome,
+      int idade = 34
+    )
+    {
+      return nome + " " + sobrenome + "tem" + idade.ToString();
+    }
+  }
+}
