e2691e4d87e4bc1ef93b3cc0963dff1e358f8c3a
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-03-30 16:53:11 -0300

    Aprendendo estrutura condicional Switch

---
+++ Estudos C# em código/UsandoSwitch/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UsandoSwitch
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      string valor = "paulo";
+      switch (valor)
+      {
+        case "joao": Console.WriteLine("Não é o cara"); break;
+        case "marcelo": Console.WriteLine("Não é o cara"); break;
+        case "andre": Console.WriteLine("É este cara"); break;
+        default: Console.WriteLine("Não encontrei"); break;
+      }
+    }
+  }
+}
