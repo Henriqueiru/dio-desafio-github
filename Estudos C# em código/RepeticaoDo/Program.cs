4cd0976bdf6d2ce7575fdd55d0455aa2dd6786e1
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-03-30 17:29:38 -0300

    Aprendendo laço de repetição DO

---
+++ Estudos C# em código/RepeticaoDo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RepeticaoDo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int valor = 0;
+
+
+
+      do
+      {
+        Console.WriteLine(valor);
+        valor++;
+
+      } while (valor < 5);
+    }
+  }
+}
