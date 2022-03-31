9ea31a64d5ac277331aefd5b04edcd3172e2bbda
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-03-31 15:15:16 -0300

    APrendendo enums

---
+++ Estudos C# em código/Enums/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Enums
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var mouse = new Product(1, "Mouse gamer", 299.97, EProductType.Product);
+      var manutencaoEletrica = new Product(2, "Manutenção elétrica residencial", 500, EProductType.Service);
+
+      mouse.Id = 55;
+
+      Console.WriteLine(mouse.Id);
+      Console.WriteLine(mouse.Name);
+      Console.WriteLine(mouse.Price);
+      Console.WriteLine((int)mouse.Type);
+    }
+
+  }
+
+  struct Product
+  {
+    public Product(int id, string name, double price, EProductType type)
+    {
+      Id = id;
+      Name = name;
+      Price = price;
+      Type = type;
+    }
+
+    public int Id;
+    public string Name;
+    public double Price;
+    public EProductType Type;
+    public double PriceInDolar(double dolar)
+    {
+
+      return Price * dolar;
+    }
+
+
+  }
+  enum EProductType
+  {
+    Product = 1,
+    Service = 2
+  }
+}
