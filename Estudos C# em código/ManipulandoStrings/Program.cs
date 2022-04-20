bbb764749724ec11a21d6d4cc46e46d3d178e2fe
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-04-20 16:12:12 -0300

    Manipulando strings

--- Estudos C# em código/ManipulandoStrings/Program.cs
+++ Estudos C# em código/ManipulandoStrings/Program.cs
@@ -10,6 +10,14 @@ namespace ManipulandoStrings
       var texto = "Este texto é um teste";
       Console.WriteLine(texto.Replace("e", "X"));
 
+      var divisao = texto.Split(" ");
+      Console.WriteLine(divisao[0]);
+      Console.WriteLine(divisao[1]);
+      Console.WriteLine(divisao[2]);
+      Console.WriteLine(divisao[3]);
+
+      var resultado = texto.Substring(5, 5);
+      Console.WriteLine(resultado);
     }
   }
 }
