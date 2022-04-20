1318fbac0424aac1641f6e0874d70ed96cf87996
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-04-20 15:51:05 -0300

    Equals added

--- Estudos C# em código/Equals/Program.cs
+++ Estudos C# em código/Equals/Program.cs
@@ -1,9 +1,16 @@
+using System.Globalization;
 using System;
 
 namespace Equals
 {
-    public class Program
+  public class Program
+  {
+    static void Main(string[] args)
     {
-        
+      var texto = "Este texto é um teste";
+      Console.WriteLine(texto.Equals("Este texto é um teste"));
+      Console.WriteLine(texto.Equals("este texto é um teste"));
+      Console.WriteLine(texto.Equals("este texto é um teste", StringComparison.OrdinalIgnoreCase));
     }
+  }
 }
