06d2130548d686a31c3b4cfc5f06e37396c01a34
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-04-20 16:05:21 -0300

    Added ToLowerToUpper

---
+++ Estudos C# em código/ToLowerToUpper/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System;
+
+namespace ToLowerToUpper
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      var texto = "Este texto é um teste";
+      Console.WriteLine(texto.ToLower());
+      Console.WriteLine(texto.ToUpper());
+      Console.WriteLine(texto.Insert(5, "AQUI"));
+      Console.WriteLine(texto.Remove(5, 5));
+      Console.WriteLine(texto.Length);
+    }
+  }
+}
