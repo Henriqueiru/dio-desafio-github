9a9507161c2ef8ae2674fac70ccf59b3d1eefc7b
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-04-20 16:08:19 -0300

    Replace

---
+++ Estudos C# em código/ManipulandoStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using System;
+
+namespace ManipulandoStrings
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      var texto = "Este texto é um teste";
+      Console.WriteLine(texto.Replace("e", "X"));
+
+    }
+  }
+}
