cbf1072bedce3a5d1a28cf5a63c7962baebbe3b0
Henriqueiru <henrique-lima-neves@hotmail.com>
2022-05-01 23:02:10 -0300

    New studies

---
+++ Estudos C# em código/TestesMetodos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestesMetodos
+{
+  public class Program
+  {
+    static string NewPessoa(string nome, string sobrenome)
+    {
+      return nome + " " + sobrenome;
+    }
+    static void Main(string[] args)
+    {
+      var person = NewPessoa("Henrique", "Lima");
+      Console.WriteLine(person);
+    }
+
+
+  }
+}
